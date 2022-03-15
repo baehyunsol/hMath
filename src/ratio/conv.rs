3c86d1b95cffed5122e44de6a575625d35db7e5a
baehyunsol <47506131+baehyunsol@users.noreply.github.com>
2022-03-15 12:01:51 +0900

    New Ratio conversion functions

--- src/ratio/conv.rs
+++ src/ratio/conv.rs
@@ -13,6 +13,16 @@ impl Ratio {
     }
 
     #[inline]
+    pub fn from_u64s(denom: u64, numer: u64) -> Ratio {
+        Ratio::new(BigInt::from_u64(denom), BigInt::from_u64(numer))
+    }
+
+    #[inline]
+    pub fn from_u64(n: u64) -> Ratio {
+        Ratio {denom: BigInt::one(), numer: BigInt::from_u64(n)}
+    }
+
+    #[inline]
     pub fn from_i32s(denom: i32, numer: i32) -> Ratio {
         Ratio::new(BigInt::from_i32(denom), BigInt::from_i32(numer))
     }
@@ -23,6 +33,16 @@ impl Ratio {
     }
 
     #[inline]
+    pub fn from_i64s(denom: i64, numer: i64) -> Ratio {
+        Ratio::new(BigInt::from_i64(denom), BigInt::from_i64(numer))
+    }
+
+    #[inline]
+    pub fn from_i64(n: i64) -> Ratio {
+        Ratio {denom: BigInt::one(), numer: BigInt::from_i64(n)}
+    }
+
+    #[inline]
     pub fn from_big_int(n: BigInt) -> Ratio {
         Ratio {denom: BigInt::one(), numer: n}
     }
@@ -30,6 +50,10 @@ impl Ratio {
     #[inline]
     pub fn from_big_ints(denom: BigInt, numer: BigInt) -> Ratio {
         Ratio::new(denom, numer)
+    }
+
+    pub fn from_string(s: String) -> Result<Ratio, &'static str> {
+        panic!("Not Implemented!")
     }
 
 }
